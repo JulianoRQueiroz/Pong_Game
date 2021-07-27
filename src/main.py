68db515bab8d0b99c786c3725c9f6be74fa6cbcf
JulianoQueiroz <juliano.invest@outlook.com>
2021-07-27 10:48:36 -0300

    Configurando pygame

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import pygame
+
+# Constantes e variáeis 
+WHITE = (255, 255, 255) # Raquetes 
+BLACK = (0, 0, 0)       # Background
+
+WIDTH = 600     # Largura 
+HEIGHT = 600    # Comprimento
+
+pygame.init()
+game_font = pygame.font.SysFont('Ubuntu', 40)
